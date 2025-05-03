fac7a9c816a214f2f46f61230cd32cd070781f3e
Charlie Bignell <charlie-bignell@outlook.com>
2025-05-03 14:22:15 +0100

    Final final Update

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ var x = setInterval(function () {
         // If the count down is finished, write some text
         if (distance < 0) {
             clearInterval(x);
-            document.getElementById("countdown").innerHTML = "TODAY'S THE DAY!";
+            document.getElementById("countdown").innerHTML = "It's been and gone, why are you still here?";
         }
     }
 
